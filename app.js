24482f3391523abdeaadafcf55266dc7ae0da777
Kartikey Varshney <kartikeyvarshney5252@gmail.com>
2024-07-14 17:05:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const api = "https://dog.ceo/api/breeds/image/random";
+
+  try {
+    const response = await fetch(api);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    res.send(`<div style="height:100vh; width:100vw; display:grid; place-items:center">
+      <img src=${result.message} style="height:500px ; width:500px" />
+      </div>
+      `);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(5000, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Server is online at http://localhost:5000");
+  }
+});
